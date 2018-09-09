84a8087572ec1ab60ec663c33dac60fbbc7c6fae
Kojo Hinson <frescodevmgmt@gmail.com>
2018-09-09 15:51:44 +0100

    Initial commit

---
+++ build-contract.js
@@ -0,0 +1,12 @@
+const { contract } = require("../package.json");
+const path = require("path");
+const fs = require("fs");
+const filePath = path.join(__dirname, "../spec.json");
+const contractJSON = JSON.stringify(contract);
+fs.writeFile(filePath, contractJSON, function(err) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("The spec was created!");
+});
